7ec0733b31ce13c86e9006f852b0822f893daf49
Myers <mmbonda@lcs.on.ca>
2024-01-23 14:09:27 -0500

    Initial Commit

---
+++ tradingcard/tradingcardApp.swift
@@ -0,0 +1,17 @@
+//
+//  tradingcardApp.swift
+//  tradingcard
+//
+//  Created by Myers Elliott Mbonda on 2024-01-23.
+//
+
+import SwiftUI
+
+@main
+struct tradingcardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
